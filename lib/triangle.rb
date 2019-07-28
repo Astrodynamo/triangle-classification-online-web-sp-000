3228c3d1c33a447ebb9ea117584a5e5a6e1cf1c3
Amanda King <amapickell@gmail.com>
2019-07-28 21:43:08 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   
   def kind
     case triangle
-    when @side_1 == @side_2 == @side_3
+    when @side_1 == @side_2 && @side_2 == @side_3
       return :equilateral
     end
   end
